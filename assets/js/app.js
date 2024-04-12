8fc9e64f9d2f715f0e67a05f2639fb2be3bad692
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-04-12 10:01:54 +0530

    make hidden slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -144,7 +144,7 @@ $('.slick_slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: ".prev",
-    nextArrow:".next",
+    nextArrow: ".next",
     responsive: [
         {
             breakpoint: 1024,
@@ -153,6 +153,43 @@ $('.slick_slider').slick({
                 slidesToScroll: 1,
                 infinite: true,
                 dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+// hidden img slider
+$('.media_screen_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
             }
         },
         {
